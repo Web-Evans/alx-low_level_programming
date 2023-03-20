7e0a667a574ec6cebb930575fe9d952d42bae747
Web-Evans <oseimail.eob@gmail.com>
2023-03-20 10:52:08 -0700

    printf Function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Function that wirtes  proper grammer
+ * Return: 0 sucessful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
