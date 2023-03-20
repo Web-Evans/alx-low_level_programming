0b3a74b8ccf6841134b8b3015347e61ac77699a4
Web-Evans <oseimail.eob@gmail.com>
2023-03-20 14:24:33 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - c code that print without printf and puts
+ * Return: 0 when successful
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
+
