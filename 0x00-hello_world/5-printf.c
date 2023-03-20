b44075b58f36fe163ff306100e190ececf8a6444
Web-Evans <oseimail.eob@gmail.com>
2023-03-20 11:03:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Function that wirtes  proper grammer
+ *
  * Return: 0 sucessful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+printf("with proper grammar, but the outclme is a piece of art,");
+return (0);
 }
