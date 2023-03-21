77dbbea45d00426fb7bb4baaf6af46a166e792f7
Web-Evans <oseimail.eob@gmail.com>
2023-03-20 19:37:47 -0700

    single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 0; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
