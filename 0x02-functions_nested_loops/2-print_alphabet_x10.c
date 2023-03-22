3fbb5da7f8e06898e82670b7ebdbd901b19aac6c
Web-Evans <oseimail.eob@gmail.com>
2023-03-21 17:02:05 -0700

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - alphabet 10x
+ * Return: 0 when succeed
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+		_putchar('\n');
+	}
+}
+
