145cf43ff71f4e2251464116cf41e56f04c64b1e
web-evans <oseimail.eob@gmail.com>
2023-06-26 14:38:55 -0700

    0-print_name.c task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - print name using pointer to function
+* @name: string to add
+* @b: pointer to function
+* Return: nothing
+**/
+void print_name(char *name, void (*b)(char *))
+{
+	if (name == NULL || b == NULL)
+		return;
+
+
+		b(name);
+}
